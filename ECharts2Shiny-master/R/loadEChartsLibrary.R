2feb027767d204fe8a4f109a7e5a6ddca2bf0af3
Xiaodong <XD-DENG@users.noreply.github.com>
2016-06-10 12:09:08 +0800

    Futher enhancement to check if there is "www" directory

--- ECharts2Shiny-master/R/loadEChartsLibrary.R
+++ ECharts2Shiny-master/R/loadEChartsLibrary.R
@@ -1,6 +1,10 @@
 loadEChartsLibrary <- function(running_in_shiny = TRUE){
 
   # Copy the JS library file to the Shiny App "www" folder
+  if(dir.exists("www/") == FALSE){
+    dir.create("www/")
+  }
+
   lib_path <- system.file("echarts.js", package = "ECharts2Shiny")
   file.copy(lib_path, "www/echarts.js")
 
