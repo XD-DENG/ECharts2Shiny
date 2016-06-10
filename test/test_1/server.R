bc5c3cc5d1d96034f5c108f4abb6857cc6e67e0f
XD-DENG <xd_deng@hotmail.com>
2016-06-10 23:55:02 +0800

    Small Tweak

--- test/test_1/server.R
+++ test/test_1/server.R
@@ -1,27 +1,33 @@
 library(shiny)
 library(ECharts2Shiny)
 
+
+
+# Prepare sample data for plotting ---------------------------------------
+
 dat <- data.frame(a=c("A", "B", "C"),
                   b=c(1, 2, 3))
 
-dat_line <- data.frame(c(1, 2, 3),
-                  c(2, 4, 6),
-                  c(3, 2, 7))
+dat_line <- data.frame(c(1, 2, 3, 1),
+                  c(2, 4, 6, 6),
+                  c(3, 2, 7, 5))
 
 names(dat_line) <- c("Type-A", "Type-B", "Type-C")
-row.names(dat_line) <- c("Time-1", "Time-2", "Time-3")
+row.names(dat_line) <- c("Time-1", "Time-2", "Time-3", "Time-4")
+
+
+
 
 
 shinyServer(function(input, output) {
 
-  # Call renderPieChart function to render charts
+  # Call functions from ECharts2Shiny to render charts
   renderPieChart(div_id = "test_1",
                  data = dat,
                  radius = "70%",center_x = "50%", center_y = "50%")
 
-  renderPieChart(div_id = "test_2",
-                 data = dat,
-                 radius = "50%",center_x = "50%", center_y = "50%")
+  renderLineChart(div_id = "test_2",
+                  data = dat_line)
 
   renderBarChart(div_id = "test_3",
                  data = dat)
@@ -30,8 +36,7 @@ shinyServer(function(input, output) {
                  direction = "vertical",
                  data = dat)
 
-  renderLineChart(div_id = "test_5",
-                  data = dat_line)
+
 
 
   })
--- test/test_1/ui.R
+++ test/test_1/ui.R
@@ -3,19 +3,17 @@ library(ECharts2Shiny)
 
 
 shinyUI(fluidPage(
-
   # We HAVE TO to load the ECharts javascript library in advance
-
   loadEChartsLibrary(),
 
   fluidRow(
     column(6,
-           tags$div(id="test_1", style="width: '50%';height:300px;"),  # Specify the div for the chart. Can also be considered as a space holder
+           tags$div(id="test_1", style="width: '80%';height:300px;"),  # Specify the div for the chart. Can also be considered as a space holder
            deliverChart(div_id = "test_1")  # Deliver the plotting
            ),
     column(6,
-           tags$div(id="test_2", style="width: '80%';height:200px;"),  # Specify the div for the chart. Can also be considered as a space holder
-           deliverChart(div_id = "test_2")  # Deliver the plotting
+           tags$div(id="test_2", style="width:80%;height:300px;"),
+           deliverChart(div_id = "test_2")
            )
   ),
 
@@ -28,12 +26,7 @@ shinyUI(fluidPage(
            tags$div(id="test_4", style="width:80%;height:300px;"),
            deliverChart(div_id = "test_4")
            )
-  ),
-
-  tags$div(id="test_5", style="width:80%;height:300px;"),
-  deliverChart(div_id = "test_5")
-
-
+  )
 
 
 )
