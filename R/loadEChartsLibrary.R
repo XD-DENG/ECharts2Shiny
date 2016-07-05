02855b9d8912752f98f480ec97d8b14cf74c13d8
Xiaodong <XD-DENG@users.noreply.github.com>
2016-07-05 19:02:30 +0800

    Big Advance-no "www" copy folder is needed anymore
    Now we use includeScript() function which can help us include the JS script file into the Shiny app. This is a big progress, as we don't need to copy the JS library files into the users' shiny app anymore, nor build a "www" folder in users' folder.

--- R/loadEChartsLibrary.R
+++ R/loadEChartsLibrary.R
@@ -1,19 +1,8 @@
-loadEChartsLibrary <- function(running_in_shiny = TRUE){
+loadEChartsLibrary <- function(){
+  
+  to_eval='includeScript(system.file("echarts.js", package = "ECharts2Shiny"))'
+  
+  eval(parse(text = to_eval), envir = parent.frame())
 
-  # Copy the JS library file to the Shiny App "www" folder
-  if(dir.exists("www/") == FALSE){
-    dir.create("www/")
-  }
-
-  lib_path <- system.file("echarts.js", package = "ECharts2Shiny")
-  file.copy(lib_path, "www/echarts.js")
-
-  to_eval <- paste('tags$script(src="echarts.js")')
-
-  if(running_in_shiny == TRUE){
-    eval(parse(text = to_eval), envir = parent.frame())
-  } else {
-    cat(to_eval)
-  }
 }
 
