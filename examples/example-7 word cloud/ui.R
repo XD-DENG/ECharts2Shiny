f8fe3270c1918c8dcbb8da8d065592adfdd22fb7
Xiaodong <XD-DENG@users.noreply.github.com>
2016-07-15 18:00:13 +0800

    Modify the typo in div of new example

--- examples/example-7 word cloud/ui.R
+++ examples/example-7 word cloud/ui.R
@@ -10,7 +10,7 @@ shinyUI(fluidPage(
 
   h2("Population Ranking Top 10"),
   "Data Source: http://www.infoplease.com/world/statistics/most-populous-countries.html",
-  tags$div(id="test", style="width:'80%';height:600px;"),  # Specify the div for the chart. Can also be considered as a space holder
+  tags$div(id="test", style="width:30%;height:600px;"),  # Specify the div for the chart. Can also be considered as a space holder
   deliverChart(div_id = "test")  # Deliver the plotting
 
 
