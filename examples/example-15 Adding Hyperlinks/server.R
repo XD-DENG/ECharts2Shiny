7773cedf8f8e3496b140ab0e05cb9873107fb1fd
XD-DENG <xd_deng@hotmail.com>
2016-11-09 23:03:55 +0800

    "hyperlinks" feature supports pie charts now

---
+++ examples/example-15 Adding Hyperlinks/server.R
@@ -0,0 +1,49 @@
+library(shiny)
+library(ECharts2Shiny)
+
+
+
+# Prepare sample data for plotting ---------------------------------------
+
+dat_1 <- data.frame(name=c("Google", "Bing", "Baidu"),
+                    value=c(8, 5, 1))
+
+dat_2 <- data.frame(c(1, 2, 3, 1),
+                  c(2, 4, 6, 6),
+                  c(3, 2, 7, 5))
+names(dat_2) <- c("Type-A", "Type-B", "Type-C")
+row.names(dat_2) <- c("Time-1", "Time-2", "Time-3", "Time-4")
+
+
+
+
+
+
+shinyServer(function(input, output) {
+
+  # Call functions from ECharts2Shiny to render charts
+  renderPieChart(div_id = "test_1",
+                 data = dat_1,
+                 radius = "70%",center_x = "50%", center_y = "50%",
+                 hyperlinks = c("https://google.com",
+                                "https://bing.com",
+                                "https://www.baidu.com"))
+
+
+  renderBarChart(div_id = "test_3", grid_left = '1%',
+                 data = dat_2,
+                 hyperlinks = c("http://me.seekingqed.com",
+                                "https://google.com",
+                                "https://github.com",
+                                "http://www.bjfu.edu.cn"))
+
+  renderBarChart(div_id = "test_4", theme = "vintage",
+                 direction = "vertical", grid_left = "10%",
+                 data = dat_2,
+                 hyperlinks = c("http://me.seekingqed.com",
+                                "https://google.com",
+                                "https://github.com",
+                                "http://www.bjfu.edu.cn"))
+
+
+  })
---
+++ examples/example-15 Adding Hyperlinks/ui.R
@@ -0,0 +1,33 @@
+library(shiny)
+library(ECharts2Shiny)
+
+
+shinyUI(fluidPage(
+  # We HAVE TO to load the ECharts javascript library in advance
+  loadEChartsLibrary(),
+  loadEChartsTheme('shine'),
+  loadEChartsTheme('vintage'),
+
+  fluidRow(
+    column(6,
+           tags$div(id="test_1", style="width:100%;height:300px;"),  # Specify the div for the chart. Can also be considered as a space holder
+           deliverChart(div_id = "test_1")  # Deliver the plotting
+           )
+  ),
+
+  fluidRow(
+    column(6,
+           tags$div(id="test_3", style="width:100%;height:300px;"),
+           deliverChart(div_id = "test_3")
+           ),
+    column(6,
+           tags$div(id="test_4", style="width:100%;height:300px;"),
+           deliverChart(div_id = "test_4")
+           )
+  )
+
+
+
+
+)
+)
