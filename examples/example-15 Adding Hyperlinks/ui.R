49ce0eb7163d819d69492601bc9731c6461931b7
XD-DENG <xd_deng@hotmail.com>
2016-12-05 22:41:56 +0800

    refine the example for new feature "hyperlinks"

--- examples/example-15 Adding Hyperlinks/ui.R
+++ examples/example-15 Adding Hyperlinks/ui.R
@@ -5,38 +5,30 @@ library(ECharts2Shiny)
 shinyUI(fluidPage(
   # We HAVE TO to load the ECharts javascript library in advance
   loadEChartsLibrary(),
-  loadEChartsTheme('shine'),
-  loadEChartsTheme('vintage'),
 
   h2("Adding Hyperlinks to Your Interactive Charts"),
 
   wellPanel(
-    p("You can refer to the documentation of pie chart, bar chart, and word cloud for this feature."),
-    p("Please note, if hyperlinks are added into your charts, the fonts in the pop-up window will be in skyblue color and italic style.")
+    p("The 'hyperlinks' feature is only supported in pie chart, bar chart, and word cloud for this feature."),
+    p("Please note, if hyperlinks are added into your charts, the fonts in the pop-up window will be in skyblue color and italic style, like", 
+      em(span("hyperlinks", style = "color:skyblue")), 
+      ". Then Users can click on the element to be redirected to the URL specified.")
     ),
 
   fluidRow(
-    column(6,
+    column(4,
            tags$div(id="test_1", style="width:100%;height:300px;"),  # Specify the div for the chart. Can also be considered as a space holder
            deliverChart(div_id = "test_1")  # Deliver the plotting
-           )
-  ),
-
-  fluidRow(
-    column(6,
+           ),
+    
+    column(4,
            tags$div(id="test_2", style="width:100%;height:300px;"),
            deliverChart(div_id = "test_2")
            ),
-    column(6,
-           tags$div(id="test_3", style="width:100%;height:300px;"),
-           deliverChart(div_id = "test_3")
-           )
-  ),
-
-  fluidRow(
-    column(6,
-           tags$div(id="test_4", style="width:80%;height:250px;"),  # Specify the div for the chart. Can also be considered as a space holder
-           deliverChart(div_id = "test_4")  # Deliver the plotting
+    
+    column(4,
+           tags$div(id="test_3", style="width:80%;height:250px;"),  # Specify the div for the chart. Can also be considered as a space holder
+           deliverChart(div_id = "test_3")  # Deliver the plotting
     )
   )
 
