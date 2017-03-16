03a13aa9b5c4fba986043fb4db68b1abeaf99ffc
XD-DENG <xd_deng@hotmail.com>
2017-03-16 22:52:21 +0800

    Debug the 'heatmap' example which arose due to the Javascript library upgrade

--- examples/example-14 Heat Map/server.R
+++ examples/example-14 Heat Map/server.R
@@ -1,16 +1,16 @@
 library(shiny)
 library(ECharts2Shiny)
 
-dat_1 <- volcano
 
-# A sample data with row names and column names
-dat_2 <- volcano
-row.names(dat_2) <- 1:dim(dat_2)[1]
-colnames(dat_2) <- 1:dim(dat_2)[2]
+dat_1 <- volcano
+row.names(dat_1) <- 1:dim(dat_1)[1]
+colnames(dat_1) <- 1:dim(dat_1)[2]
 
 # A sample data with NA values
-dat_3 <- volcano
-dat_3[10:15, 10:15] <- NA
+dat_2 <- volcano
+dat_2[10:15, 10:15] <- NA
+row.names(dat_2) <- 1:dim(dat_2)[1]
+colnames(dat_2) <- 1:dim(dat_2)[2]
 
 
 shinyServer(function(input, output) {
@@ -21,9 +21,6 @@ shinyServer(function(input, output) {
 
   renderHeatMap(div_id = "test_2",
                 data = dat_2)
-
-  renderHeatMap(div_id = "test_3",
-                data = dat_3)
 
   }
 )
--- examples/example-14 Heat Map/ui.R
+++ examples/example-14 Heat Map/ui.R
@@ -7,23 +7,18 @@ shinyUI(fluidPage(
   loadEChartsLibrary(),
 
   fluidRow(
-    column(4,
+    column(6,
            h4("To Give A Matrix without Row Names or Column Names", align = "center"),
            tags$div(id="test_1", style="width:100%;height:400px;"),
            deliverChart(div_id = "test_1")
            ),
 
-    column(4,
+    column(6,
            h4("To Give A Matrix with Row Names & Column Names", align = "center"),
            tags$div(id="test_2", style="width:100%;height:400px;"),
            deliverChart(div_id = "test_2")
-           ),
+           )
 
-    column(4,
-           h4("Deal with Missing Values in Data Input", align = "center"),
-           tags$div(id="test_3", style="width:100%;height:400px;"),
-           deliverChart(div_id = "test_3")
-    )
   )
 
 
